e9ad61d85460a9d5773a535ab8199fd68c40f174
Aviano <avanast08@googlemail.com>
2021-11-07 13:18:46 -0800

    created simple repo list controller

--- src/Controller/RepoController.php
+++ src/Controller/RepoController.php
@@ -1,20 +1,31 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Controller;
 
+use App\Entity\CodeRepo;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
-
 
 class RepoController extends AbstractController
 {
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/repo', name: 'repo')]
     public function index(): Response
     {
+        $repositories = $this->entityManager->getRepository(CodeRepo::class)->findAll();
+
         return $this->render('repo/index.html.twig', [
-            'controller_name' => 'RepoController',
+            'repositories' => $repositories,
         ]);
     }
 }
