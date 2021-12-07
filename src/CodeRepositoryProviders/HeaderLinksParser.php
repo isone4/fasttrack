284a999580408ef60fd31b9f4b3bcdc1605e354d
isone4 <avanast08@googlemail.com>
2021-12-07 10:46:21 +0100

    extracted method to fetch headerlinks to new class + added provider selection

---
+++ src/CodeRepositoryProviders/HeaderLinksParser.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\CodeRepositoryProviders;
+
+class HeaderLinksParser
+{
+    private readonly array $explodedLinks;
+
+    /**
+     * @param array $header
+     */
+    public function __construct(array $header)
+    {
+        $this->explodedLinks = explode(",", ($header['link']['0'] ?? []));
+    }
+
+    public function headerLinks(): array
+    {
+        $headerlinks = [];
+        foreach ($this->explodedLinks as $explodedlink) {
+            $explodedlink = trim($explodedlink);
+            $beginning = strpos($explodedlink, '<') + 1;
+            $end = strpos($explodedlink, '>') - 1;
+            $url = substr($explodedlink, $beginning, $end);
+            $linktype = strpos($explodedlink, 'rel=') + 5;
+            $type = substr($explodedlink, $linktype, -1);
+            $headerlinks[$type] = $url;
+        }
+
+        return $headerlinks;
+    }
+}
